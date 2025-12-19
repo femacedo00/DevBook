e15a15b46f2fcd76f18ba7d8c848e9c7e61ad431
Felipe <felipe.macedo00@gmail.com>
2025-12-19 17:16:22 -0300

    Token Validation

--- api/src/authentication/token.go
+++ api/src/authentication/token.go
@@ -2,6 +2,10 @@ package authentication
 
 import (
 	"api/src/config"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -15,4 +19,38 @@ func CreateToken(userID uint64) (string, error) {
 	permissions["userId"] = userID
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
 	return token.SignedString([]byte(config.SecretKey))
+}
+
+// ValidateToken checks if the request token is validated
+func ValidateToken(r *http.Request) error {
+	tokenString := extractToken(r)
+	token, error := jwt.Parse(tokenString, returnKeyVerification)
+	if error != nil {
+		return error
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok || !token.Valid {
+		return errors.New("Invalid token")
+	}
+
+	return nil
+}
+
+func extractToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+	split := strings.Split(token, " ")
+
+	if len(split) == 2 {
+		return split[1]
+	}
+
+	return ""
+}
+
+func returnKeyVerification(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected signature method! %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
--- api/src/middlewares/middlewares.go
+++ api/src/middlewares/middlewares.go
@@ -1,23 +1,27 @@
 package middlewares
 
 import (
-	"fmt"
+	"api/src/authentication"
+	"api/src/response"
 	"log"
 	"net/http"
 )
 
 // Logger writes request information to the terminal
-func Logger(next http.HandlerFunc) http.HandlerFunc {
+func Logger(nextFunc http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
-		next(w, r)
+		nextFunc(w, r)
 	}
 }
 
 // Authenticate checks if user is authenticated
-func Atuthenticate(next http.HandlerFunc) http.HandlerFunc {
+func Atuthenticate(nextFunc http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Atuthenticating...")
-		next(w, r)
+		if error := authentication.ValidateToken(r); error != nil {
+			response.Error(w, http.StatusUnauthorized, error)
+			return
+		}
+		nextFunc(w, r)
 	}
 }

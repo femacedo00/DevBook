d77d17a96f61cf4e13a6422de9cc75e0dab3bac3
Felipe <felipe.macedo00@gmail.com>
2026-01-13 16:47:20 -0300

    Render Publications On Home Page

--- webapp/src/controllers/pages.go
+++ webapp/src/controllers/pages.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/models"
 	"webapp/src/request"
+	"webapp/src/response"
 	"webapp/src/utils"
 )
 
@@ -21,8 +24,23 @@ func LoadUserRegisterPage(w http.ResponseWriter, r *http.Request) {
 // LoadHomePage loads home page
 func LoadHomePage(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publications", config.APIURL)
-	response, error := request.RequestWithAuth(r, http.MethodGet, url, nil)
-	fmt.Println(response, error)
+	getResponse, error := request.RequestWithAuth(r, http.MethodGet, url, nil)
+	if error != nil {
+		response.JSON(w, http.StatusInternalServerError, response.ErrorAPI{Error: error.Error()})
+		return
+	}
+	defer getResponse.Body.Close()
 
-	utils.ExecuteHtmlTemplate(w, "home.html", nil)
+	if getResponse.StatusCode >= 400 {
+		response.HandleErrorStatusCode(w, getResponse)
+		return
+	}
+
+	var publications []models.Publication
+	if error = json.NewDecoder(getResponse.Body).Decode(&publications); error != nil {
+		response.JSON(w, http.StatusUnprocessableEntity, response.ErrorAPI{Error: error.Error()})
+		return
+	}
+
+	utils.ExecuteHtmlTemplate(w, "home.html", publications)
 }

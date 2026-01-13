2796a71282cfaea75bbecd4e4e2de49678aa7cd4
Felipe <felipe.macedo00@gmail.com>
2026-01-13 18:47:37 -0300

    NavBar And Templates

--- webapp/src/utils/templates.go
+++ webapp/src/utils/templates.go
@@ -10,6 +10,7 @@ var templates *template.Template
 // LoadTemplates parses all HTML templates and stores them in the templates variable
 func LoadTemplates() {
 	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/templates/*.html"))
 }
 
 // ExecuteHtmlTemplate renders an html page to the HTTP response

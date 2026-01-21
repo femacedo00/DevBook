d54cf939f07c2b246e729bb2868cb99b2cae5b53
Felipe <felipe.macedo00@gmail.com>
2026-01-21 09:34:30 -0300

    Logged-in User Validation

--- webapp/src/controllers/pages.go
+++ webapp/src/controllers/pages.go
@@ -17,11 +17,25 @@ import (
 
 // LoadLoginPage loads login page
 func LoadLoginPage(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := cookies.Read(r)
+
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", 302)
+		return
+	}
+
 	utils.ExecuteHtmlTemplate(w, "login.html", nil)
 }
 
 // LoadUserRegisterPage loads user register page
 func LoadUserRegisterPage(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := cookies.Read(r)
+
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", 302)
+		return
+	}
+
 	utils.ExecuteHtmlTemplate(w, "register.html", nil)
 }
 
